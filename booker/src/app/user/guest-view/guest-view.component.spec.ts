bdfbf33fcb4c3c64fca716b9d49413733e63cca1
matejicdunja <dunja2002matejic@gmail.com>
2024-01-20 23:25:26 +0100

    setting the component to run initial test

--- booker/src/app/user/guest-view/guest-view.component.spec.ts
+++ booker/src/app/user/guest-view/guest-view.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GuestViewComponent } from './guest-view.component';
+import {HttpClientModule} from "@angular/common/http";
+import {MatIconModule} from "@angular/material/icon";
+import {FormsModule} from "@angular/forms";
 
 describe('GuestViewComponent', () => {
   let component: GuestViewComponent;
@@ -8,7 +11,8 @@ describe('GuestViewComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [GuestViewComponent]
+      declarations: [GuestViewComponent],
+      imports: [HttpClientModule, MatIconModule, FormsModule]
     });
     fixture = TestBed.createComponent(GuestViewComponent);
     component = fixture.componentInstance;
